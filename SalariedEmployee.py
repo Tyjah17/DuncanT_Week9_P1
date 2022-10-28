05f546decb9f659bc059503165f102f4d35d5b05
Tyjah17 <70492259+Tyjah17@users.noreply.github.com>
2022-10-28 11:51:52 -0600

    Update SalariedEmployee.py

--- SalariedEmployee.py
+++ SalariedEmployee.py
@@ -1,13 +1,13 @@
 from Employee import Employee
 
-
+# 
 class SalariedEmployee(Employee):
     def __init__(self, f_name, l_name, ssn, employee_id, monthly_salary):
         super().__init__(f_name, l_name, ssn, employee_id)
         self.monthly_salary = monthly_salary
-
+    # returns users monthly salary
     def get_monthly_salary(self):
         return self.monthly_salary
-
+    # display user first and last name, employee_id and monthly_salary
     def __str__(self):
         return f'{super().__str__()} Salary: ${"{:.2f}".format(self.get_monthly_salary())}'
