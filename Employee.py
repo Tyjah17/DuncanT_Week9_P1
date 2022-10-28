40e0c15b5dae8d42a148654a4be88d136718d0e3
Tyjah17 <70492259+Tyjah17@users.noreply.github.com>
2022-10-28 13:10:20 -0600

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -1,10 +1,13 @@
 from Person import Person
 
-# this class is a child class of the class Person
+
+# this class inherits properties f_name, l_name, and ssn from Person class
+# a new property employee_id is made
 class Employee(Person):
     def __init__(self, f_name, l_name, ssn, employee_id):
         super().__init__(f_name, l_name, ssn)
         self.employee_id = employee_id
 
+    # prints first and last name, ssn and employee id
     def __str__(self):
         return f'{super().__str__()}, ID: {self.employee_id}'
