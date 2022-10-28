8c00e16b5fbd50114e883bf88397b85adc6250bd
Tyjah17 <70492259+Tyjah17@users.noreply.github.com>
2022-10-28 13:11:45 -0600

    Update SalariedEmployee.py

--- SalariedEmployee.py
+++ SalariedEmployee.py
@@ -1,13 +1,17 @@
 from Employee import Employee
 
-# 
+
+# this class inherits the properties of Employee class which inherits the properties of
+# the Person class
 class SalariedEmployee(Employee):
     def __init__(self, f_name, l_name, ssn, employee_id, monthly_salary):
         super().__init__(f_name, l_name, ssn, employee_id)
         self.monthly_salary = monthly_salary
-    # returns users monthly salary
+
+    # returns monthly_salary depending on user input
     def get_monthly_salary(self):
         return self.monthly_salary
-    # display user first and last name, employee_id and monthly_salary
+
+    # str function that will display f_name, l_name, snn, employee_id, and monthly_salary
     def __str__(self):
         return f'{super().__str__()} Salary: ${"{:.2f}".format(self.get_monthly_salary())}'
