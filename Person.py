21957e99c474df7b288b09f855646397070358d4
Tyjah17 <70492259+Tyjah17@users.noreply.github.com>
2022-10-28 13:11:29 -0600

    Update Person.py

--- Person.py
+++ Person.py
@@ -1,9 +1,11 @@
 # parent class for the class Employee
 class Person:
+    # Person Constructor
     def __init__(self, f_name, l_name, ssn):
         self.f_name = str(f_name)
         self.l_name = str(l_name)
         self.ssn = str(ssn)
-    #
+
+    # prints first and last name along with ssn
     def __str__(self):
         return f'Name: {self.f_name} {self.l_name} SSN: {self.ssn}'
